d1ab1e3543a7c9fd0fdbd14ceda47fa97993d5ba
Ray Flores <ray.m.flores@gmail.com>
2021-11-02 05:19:40 +0000

    JuliaJames loop example

--- loops.js
+++ loops.js
@@ -1,6 +1,23 @@
-var x = 10;
+/*var x = 10;
 while (x <= 25) {
   console.log('Printing out x = ' + x);
   x = x + 2;
-}
+}*/
 
+var x = 1;
+
+while (x <= 20/* your stop condition goes here */) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("JuliaJames");
+    } else if (x % 5 === 0) {
+        console.log("James");
+    } else if (x % 3 === 0) {
+        console.log("Julia");
+    } else {
+        console.log(x);
+    }
+    x = x + 1;
+    // check divisibility
+    // print Julia, James, or JuliaJames
+    // increment x
+}
