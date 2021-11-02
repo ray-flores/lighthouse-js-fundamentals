353aee0f0db3b53ac282cfce332c079ce96e9619
Ray Flores <ray.m.flores@gmail.com>
2021-11-02 01:36:23 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,21 @@
-const temperature = 15;
+
+
+
+/*const raining = true;
+const cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!"); *\
+
+
+/*const temperature = 12;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -8,4 +25,26 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");*\
+
+
+/*const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}*/
+
+/*if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}*/
+
+var raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
+
+
