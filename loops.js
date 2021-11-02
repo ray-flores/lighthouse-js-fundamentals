88632e76ab1ae537a4d71343525e298c853d8a86
Ray Flores <ray.m.flores@gmail.com>
2021-11-02 06:10:44 +0000

    99 Bottles of Juice loop

--- loops.js
+++ loops.js
@@ -4,9 +4,9 @@ while (x <= 25) {
   x = x + 2;
 }*/
 
-var x = 1;
+/*var x = 1;
 
-while (x <= 20/* your stop condition goes here */) {
+while (x <= 20) {
     if (x % 3 === 0 && x % 5 === 0) {
         console.log("JuliaJames");
     } else if (x % 5 === 0) {
@@ -20,4 +20,21 @@ while (x <= 20/* your stop condition goes here */) {
     // check divisibility
     // print Julia, James, or JuliaJames
     // increment x
+}*/
+
+var num = 99;
+
+while (num > 0) {
+    if (num > 2) {
+        console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! " + "Take one down, pass it around... " + (num-1) + " bottles of juice on the wall!");
+    } else if (num === 2) {
+        console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! " + "Take one down, pass it around... " + (num-1) + " bottle of juice on the wall!");  
+    } else {
+        console.log(num + " bottle of juice on the wall! " + num + " bottle of juice! " + "Take one down, pass it around... " + (num-1) + " bottles of juice on the wall!");
+    }
+    num = num - 1;
+    // check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    // decrement num
 }
