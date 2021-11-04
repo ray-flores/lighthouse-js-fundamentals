a9caad023bc7ed053a336229fb3dece8472496d0
Ray Flores <ray.m.flores@gmail.com>
2021-11-04 02:25:14 +0000

    finalPosition challenge

---
+++ finalPosition.js
@@ -0,0 +1,23 @@
+
+
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+
+
+
+function finalPosition (moves) {
+  let x = 0;
+  let y = 0;
+  for (const move of moves) {
+    if (move === 'east') {
+      x += 1;
+    } else if (move === 'west') {
+      x -= 1;
+    } else if (move === 'north') {
+      y += 1;
+    }
+  }  
+return position = [x, y];
+}
+
+console.log(finalPosition(moves));
